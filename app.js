d718a09f835710fe478e3682002e3650ee24d051
yeg003 <yeg003@gmail.com>
2019-04-02 22:45:13 -0400

    implemented random player and user choices

--- app.js
+++ app.js
@@ -6,4 +6,32 @@ let scoreBoard_div = document.querySelector(".score-board")
 let result_div = document.querySelector(".result");
 let rock_div = document.getElementById("r");
 let paper_div = document.getElementById("p");
-let scisspors_div = document.getElementById("s");
+let scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    let choices = ['r', 'p', 's'];
+    let randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+
+function game(userChoice) {
+    let computerChoice = getComputerChoice();
+    console.log("user choice => " + userChoice);
+    console.log("computer choice => " + computerChoice);
+}
+
+
+function main() {
+    rock_div.addEventListener('click', function () {
+        game("r")
+    })
+    paper_div.addEventListener('click', function () {
+        game("p")
+    })
+    scissors_div.addEventListener('click', function () {
+        game("s")
+    })
+}
+
+main();
