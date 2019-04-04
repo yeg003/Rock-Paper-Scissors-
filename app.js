ea0dfcfd595ba34b9d298d2dadb1840d9b71691e
yeg003 <yeg003@gmail.com>
2019-04-03 21:06:49 -0400

    implemented UI features to score board

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let userScore = 0;
 let computerScore = 0;
 let userScore_span = document.getElementById("user-score");
-let computerScore_span = document.getElementById("computer-score");
+let computerScore_span = document.getElementById("comp-score");
 let scoreBoard_div = document.querySelector(".score-board")
-let result_div = document.querySelector(".result");
+let result_p = document.querySelector(".result p");
 let rock_div = document.getElementById("r");
 let paper_div = document.getElementById("p");
 let scissors_div = document.getElementById("s");
@@ -12,13 +12,51 @@ function getComputerChoice() {
     let choices = ['r', 'p', 's'];
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
+
 }
 
+function convertToWord(letter) {
+    if (letter === 'r') return 'Rock';
+    if (letter === 'p') return 'Paper'
+    return 'Scissors'
+}
+function win(userChoice, computerChoice) {
+    userScore++
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + ' beats ' + convertToWord(computerChoice) + '. You win!'
+
+}
+function lose(userChoice, computerChoice) {
+    computerScore++
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + ' loses to ' + convertToWord(computerChoice) + '. You win!'
+
+}
+function draw(userChoice, computerChoice) {
+    result_p.innerHTML = convertToWord(userChoice) + ' draws with' + ' ' + convertToWord(computerChoice)
+}
 
 function game(userChoice) {
     let computerChoice = getComputerChoice();
-    console.log("user choice => " + userChoice);
-    console.log("computer choice => " + computerChoice);
+    switch (userChoice + computerChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            win(userChoice, computerChoice)
+            break
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice, computerChoice)
+            break
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw(userChoice, computerChoice)
+            break
+    }
 }
 
 
