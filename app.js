cf52a0df2b8e5a66e66ecfe97e561e94f98e79d8
yeg003 <yeg003@gmail.com>
2019-04-04 21:37:42 -0400

    started implementing gameWon function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let result_p = document.querySelector(".result p");
 let rock_div = document.getElementById("r");
 let paper_div = document.getElementById("p");
 let scissors_div = document.getElementById("s");
-
+let choices = document.querySelector('.choices')
 function getComputerChoice() {
     let choices = ['r', 'p', 's'];
     let randomNumber = Math.floor(Math.random() * 3);
@@ -35,14 +35,14 @@ function lose(userChoice, computerChoice) {
     computerScore++
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = convertToWord(userChoice) + ' loses to ' + convertToWord(computerChoice) + '. You win!';
+    result_p.innerHTML = convertToWord(userChoice) + ' loses to ' + convertToWord(computerChoice) + '. You Lose :(';
     userChoice_div.classList.add('red-glow');
     setTimeout(function () { userChoice_div.classList.remove('red-glow') }, 400)
 
 }
 function draw(userChoice, computerChoice) {
     let userChoice_div = document.getElementById(userChoice)
-    result_p.innerHTML = convertToWord(userChoice) + ' draws with' + ' ' + convertToWord(computerChoice)
+    result_p.innerHTML = convertToWord(userChoice) + ' draws with' + ' ' + convertToWord(computerChoice) + ' .' + ' No score is added. Pick again!'
     userChoice_div.classList.add('gray-glow');
     setTimeout(function () { userChoice_div.classList.remove('gray-glow') }, 400)
 }
@@ -80,5 +80,22 @@ function main() {
         game("s")
     })
 }
+
+// function gameWon(userScore, computerScore) {
+//     if (userScore === 0 && computerScore === 0) {
+//         return result_p.innerHTML = "First Player to reach 5 wins! Good Luck!";
+
+//     } else if (userScore === 1) {
+//         return console.log("you won!")
+//         //result_p.innerHTML = userChoice + "Has won the game!. Phenomal job! :)";
+
+
+//     } else if (computerScore === 1) {
+//         return console.log("you lost")
+//         //result_p.innerHTML = computerChoice + "Has won the game. Better luck next time :(";
+//     }
+// }
+
+gameWon();
 
 main();
